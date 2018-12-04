b7b025b2b45d9763a094d4bca62318639f650520
sreekar <sreekar339@gmail.com>
2018-12-04 07:15:49 +0530

    echo commands in updateAndRestart.sh

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -8,8 +8,10 @@ rm -rf ~/xoxo-multiplayer/xoxo-multiplayer-backend
 echo "cloning the new version of repo"
 git clone git@github.com:shksa/xoxo-multiplayer-backend.git ~/xoxo-multiplayer/xoxo-multiplayer-backend
 
+echo "Stopping pm2 daemon and all other node apps managed by it (the node apps are also deleted from the process list)"
 pm2 kill 
 
+echo "Starting the pm2 daemon"
 pm2 status
 
 echo "cd'ing into the repo location"
