ef3b45860ade36b3e92313688330a56c8d879038
sreekar <sreekar339@gmail.com>
2018-12-04 07:03:46 +0530

    added -name flag to the pm2 start command,

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -2,14 +2,18 @@ echo "deploying to remote machine 139.59.93.218 - sreekar.co \n"
 
 set -e
 
+echo "deleting the old version of repo"
 rm -rf ~/xoxo-multiplayer/xoxo-multiplayer-backend
 
+echo "cloning the new version of repo"
 git clone git@github.com:shksa/xoxo-multiplayer-backend.git ~/xoxo-multiplayer/xoxo-multiplayer-backend
 
+echo "cd'ing into the repo location"
 cd ~/xoxo-multiplayer/xoxo-multiplayer-backend
 
 echo "running yarn install"
 yarn install
 
+echo "starting the app with pm2 as a daemon"
 yarn start
 
